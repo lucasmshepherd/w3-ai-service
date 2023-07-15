25922c96c6122d22be7a23ab9d7952b7cc65375c
Jared Shepherd <jared@webops.group>
2023-07-15 12:27:29 -0700

    adds 429 error code for Open API key request quota exceeded

--- routing/open-ai.js
+++ routing/open-ai.js
@@ -16,12 +16,21 @@ export default async function (EXPRESS) {
           // TODO: Build the logic after Open AI responds
           console.log(chatCompletion);
         } catch (error) {
-          if (error?.response?.status === 401) {
-            // If this occurs, check the .env file for a valid API key.
-            res.status(500).send(`[Error Code 1]: We received your question, but were unable to complete your request unfortunately.`);
+          if (error?.response?.status) {
+            switch(error.response.status) {
+              case 401:
+                // If this occurs, check the .env file for a valid API key.
+                res.status(500).send(`[Error Code 01]: We received your question, but were unable to complete your request unfortunately.`);
+                break;
+              case 429:
+                // If this occurs, you've likely exceeded your limt/quota and need to the status of the API key
+                res.status(500).send(`[Error Code 02]: We received your question, but were unable to complete your request unfortunately.`);
+                break;
+            }
           } else {
+            console.log(error.response.status, error.response.data)
             // Some other issue happened with the Open AI service that hasn't been accounted for.
-            res.status(500).send(`[Error Code 2]: We received your question, but were unable to complete your request unfortunately.`);
+            res.status(500).send(`[Error Code 99]: We received your question, but were unable to complete your request unfortunately.`);
           }
         }
       } else {
